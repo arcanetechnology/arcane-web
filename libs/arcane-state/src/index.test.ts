da272e55a4ea2206f52e240f7f21092da0af2615
Joel D'Souza <joeldsouzax@gmail.com>
2022-05-08 16:46:06 +0300

    docs(invest): added future TODO task as comments

--- libs/arcane-state/src/index.test.ts
+++ libs/arcane-state/src/index.test.ts
@@ -2,7 +2,7 @@
  * @ Author: Joel D'Souza
  * @ Create Time: 2022-05-08 16:38:11
  * @ Modified by: Joel D'Souza
- * @ Modified time: 2022-05-08 16:39:00
+ * @ Modified time: 2022-05-08 16:44:06
  * @ Description: test file for arcane-test
  *
  * @format
