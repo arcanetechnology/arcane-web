70ddba97f7305af1f461e86319ad3a45bd6888a2
Joel D'Souza <joeldsouzax@gmail.com>
2022-05-09 12:44:17 +0300

    test(arcane-state): commiting incomplete tests

--- libs/arcane-state/src/index.test.ts
+++ libs/arcane-state/src/index.test.ts
@@ -2,10 +2,27 @@
  * @ Author: Joel D'Souza
  * @ Create Time: 2022-05-08 16:38:11
  * @ Modified by: Joel D'Souza
- * @ Modified time: 2022-05-08 16:44:06
+ * @ Modified time: 2022-05-09 12:43:32
  * @ Description: test file for arcane-test
  *
  * @format
  */
+import '@testing-library/jest-dom';
+import * as React from 'react';
 
-//TODO: figure out how to test react context shit
+describe('context as redux store', () => {
+  it('should be able to a reducer and action and create a context store', () => {
+    const INCREMENT = 'INCREMENT';
+    const DECREMENT = 'DECREMENT';
+    const counterReducer = (state: number, action: any) => {
+      switch (action.type) {
+        case INCREMENT:
+          return state++;
+        case DECREMENT:
+          return state--;
+        default:
+          return state;
+      }
+    };
+  });
+});
