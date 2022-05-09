923b90c0cf656dd49dda5639c374d96e51787e69
Joel D'Souza <joeldsouzax@gmail.com>
2022-05-09 14:30:14 +0300

    feat(arcane): added index page for arcane

---
+++ apps/arcane/config.astro.ts
@@ -0,0 +1,12 @@
+/** @format */
+
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://arcane.no',
+  integrations: [react(), partytown(), sitemap()],
+});
