219a52626b9211ad6c6d16c351688d4be071530a
Joel D'Souza <joeldsouzax@gmail.com>
2022-05-11 15:43:49 +0300

    refactor(alchemy-astro): updated head title to always have arcane

---
+++ libs/arcane-components/vite.config.ts
@@ -0,0 +1,29 @@
+/** @format */
+/** @format */
+
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const config = defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'alchemy',
+      formats: ['es', 'umd'],
+      fileName: (format) => `alchemy.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
+
+export default config;
