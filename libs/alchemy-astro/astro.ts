d9b4fc1ad39ccb0ac863aa7e791397befbbd9d94
Joel D'Souza <joeldsouzax@gmail.com>
2022-05-11 14:34:11 +0300

    refactor(alchemy-astro): added typescript support

--- libs/alchemy-astro/astro.ts
+++ libs/alchemy-astro/astro.ts
@@ -1,4 +1,5 @@
 /** @format */
+
 export { default as DefaultLayout } from './layouts/DefaultLayout.astro';
 export { default as Header } from './components/Header.astro';
 export { default as Footer } from './components/Footer.astro';
