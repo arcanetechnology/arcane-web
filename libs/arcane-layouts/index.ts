2fda444fc957c6d6818be5b26cd3633a19b75553
Joel D'Souza <joeldsouzax@gmail.com>
2022-05-15 15:40:25 +0300

    refactor(arcane-layouts): renamed alchemy-astro to arcane-layouts to better reflect the purpose

--- libs/arcane-layouts/index.ts
+++ libs/arcane-layouts/index.ts
@@ -3,5 +3,3 @@
 export { default as DefaultLayout } from './src/layouts/DefaultLayout.astro';
 export { default as Header } from './src/components/Header.astro';
 export { default as Footer } from './src/components/Footer.astro';
-
-//TODO: add accessibility stuff in all components
