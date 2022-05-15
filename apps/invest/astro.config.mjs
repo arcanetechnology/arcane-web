40674ec4b6bc2f171cb11c34df6f6be1b185f474
Joel D'Souza <joeldsouzax@gmail.com>
2022-05-15 15:13:42 +0300

    feat(invest): separated cross-cutting solid-js components in its own library and add invest pages

--- apps/invest/astro.config.mjs
+++ apps/invest/astro.config.mjs
@@ -4,7 +4,10 @@ import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
 
 export default defineConfig({
+  integrations: [solid()],
   site: 'https://arcane.no',
   base: '/invest',
-  integrations: [solid()],
+  build: {
+    format: 'file',
+  },
 });
---
+++ libs/arcane-components/src/components/index.tsx
@@ -0,0 +1,5 @@
+/** @format */
+
+export { default as ThemeToggle } from './ThemeToggle';
+export { default as Authentication } from './Authentication';
+export { default as Navigation } from './Navigation';
