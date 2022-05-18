0a2f901601bf37f68c5bf04ce76cb69b23bdeef3
Joel D'Souza <joeldsouzax@gmail.com>
2022-05-18 21:14:54 +0300

    refactor(arcane-flow): made the core logic more readable

--- libs/arcane-flow/src/index.ts
+++ libs/arcane-flow/src/index.ts
@@ -2,14 +2,14 @@
  * @ Author: Joel D'Souza
  * @ Create Time: 2022-05-05 11:02:31
  * @ Modified by: Joel D'Souza
- * @ Modified time: 2022-05-17 21:54:12
+ * @ Modified time: 2022-05-18 21:11:22
  * @ Description: core business logic of arcane-flow functionality,
  * arcaneFlow function loops through various nodes on the basis of logic present in the edges.
  * ArcaneFlowBuilder makes it a bit better to configure that function.
  * @format
  */
 
-import { pipe } from 'fp-ts/lib/function';
+import { flow, pipe } from 'fp-ts/lib/function';
 import * as O from 'fp-ts/lib/Option';
 import * as E from 'fp-ts/lib/Either';
 
@@ -35,23 +35,23 @@ const ArcaneFlow = <NodeName extends string, Answer>(
   const next = (val: Answer) => {
     const dest = pipe(
       config[curr],
-      E.fromNullable(curr),
-      E.match(
-        (_) => curr,
-        (d) => {
-          return pipe(
-            O.fromNullable(
-              Object.entries(d).find((v) => (v[1] as Logic<Answer>)(val)) as [
-                NodeName,
-                Logic<Answer>
-              ]
-            ),
-            O.map((d) => d[0]),
-            O.getOrElse(() => curr)
-          );
-        }
-      )
+      O.fromNullable,
+      O.map(
+        (d) =>
+          Object.entries(d).find((v) => (v[1] as Logic<Answer>)(val)) as [
+            NodeName,
+            Logic<Answer>
+          ]
+      ),
+      O.chain(
+        flow(
+          O.fromNullable,
+          O.map((d) => d[0])
+        )
+      ),
+      O.getOrElse(() => curr)
     );
+
     if (dest !== curr) {
       history.push(curr);
       curr = dest;
