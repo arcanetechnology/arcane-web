642b3333c43dbfc6122a2804d7058da1e38f5144
Joel D'Souza <joeldsouzax@gmail.com>
2022-06-11 23:36:38 +0200

    feat(arcane-layout): added navigation dropdown

--- libs/arcane-components/src/components/Navigation.tsx
+++ libs/arcane-components/src/components/Navigation.tsx
@@ -1,13 +1,30 @@
 /** @format */
 
 import { Button } from '@arcane-web/alchemy';
-import { Component } from 'solid-js';
+import { Component, createSignal, Show } from 'solid-js';
 
 const Navigation: Component = () => {
+  const [navigate, setNavigation] = createSignal<boolean>(false);
+
   return (
-    <Button variant="tertiary" id="navigation" onClick={(e) => console.log(e)}>
-      navigate
-    </Button>
+    <>
+      <Button
+        variant="tertiary"
+        id="navigation"
+        onMouseEnter={() => setNavigation(true)}
+        onMouseLeave={() => setNavigation(false)}
+        onClick={(e) => console.log(e)}
+      >
+        navigate
+      </Button>
+      <Show when={navigate()}>
+        <div
+          style={{ border: '1px solid #000', width: '100px', height: '300px' }}
+        >
+          hello
+        </div>
+      </Show>
+    </>
   );
 };
 
