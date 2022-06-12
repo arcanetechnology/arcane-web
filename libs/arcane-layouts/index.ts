aa15c02578624c31a90ab6b8938146b3a76f3a34
Joel D'Souza <joeldsouzax@gmail.com>
2022-06-12 21:58:28 +0200

    refactor(invest): cleaned up the html files to have less tags

--- libs/arcane-layouts/index.ts
+++ libs/arcane-layouts/index.ts
@@ -1,5 +1,4 @@
 /** @format */
 
 export { default as DefaultLayout } from './src/layouts/DefaultLayout.astro';
-export { default as Header } from './src/components/Header.astro';
-export { default as Footer } from './src/components/Footer.astro';
+export { default as DashboardLayout } from './src/layouts/DashboardLayout.astro';
