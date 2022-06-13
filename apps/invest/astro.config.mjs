a3123eb7707469f85bc5d5978d72d8ff47d3a8ef
Joel D'Souza <joeldsouzax@gmail.com>
2022-06-13 17:12:45 +0200

    feat(arcane-layout): added social icons and links to footer

--- apps/invest/astro.config.mjs
+++ apps/invest/astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
   integrations: [solid()],
   site: 'https://arcane.no/',
   base: '/invest',
-  trailingSlash: "never",
+  trailingSlash: 'never',
   vite: {
     ssr: {
       external: ['svgo'],
--- apps/research/astro.config.mjs
+++ apps/research/astro.config.mjs
@@ -7,4 +7,9 @@ export default defineConfig({
   integrations: [solid()],
   site: 'https://arcane.no/',
   base: '/research',
+  vite: {
+    ssr: {
+      external: ['svgo'],
+    },
+  },
 });
