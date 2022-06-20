69c4a83101d7b6cdd602415b138013d2a129e751
Joel D'Souza <joeldsouzax@gmail.com>
2022-06-20 13:51:34 +0200

    Dev (#2)
    * refactor(invest): removed the stepper and connected the customer form with survey
    
    * fix(invest): fixed the validation issue
    
    * fix(invest): streamlined the UI for onboarding form
    
    * feat(alchemy): added scss only project for arcane design theme
    
    * fix(invest): fixed the chartjs error of not getting the context
    
    * fix(invest): removed arcane framework from astro config to make invest run
    
    * fix(invest): fixed the arcane-components build path issue
    
    * ci(invest): added yarn test to the deploy workflow
    
    * fix(invest): changed the format to directory
    
    * fix(invest): resolved conflicts for arcane-core

--- apps/arcane/astro.config.mjs
+++ apps/arcane/astro.config.mjs
@@ -2,10 +2,11 @@
 
 import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
+import arcane from '@arcane-web/arcane-core';
 
 export default defineConfig({
   site: 'https://arcane.no',
-  integrations: [solid()],
+  integrations: [solid(), arcane()],
   vite: {
     ssr: {
       external: ['svgo'],
