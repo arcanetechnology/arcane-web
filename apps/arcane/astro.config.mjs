9cdf10e0da89aa1fb4dfdea3f60837330219abef
Vihang Patil <vihang.patil@gmail.com>
2022-06-20 14:39:23 +0200

    fix(ci): fixed build error

--- apps/arcane/astro.config.mjs
+++ apps/arcane/astro.config.mjs
@@ -2,11 +2,10 @@
 
 import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
-import arcane from '@arcane-web/arcane-core';
 
 export default defineConfig({
   site: 'https://arcane.no',
-  integrations: [solid(), arcane()],
+  integrations: [solid()],
   vite: {
     ssr: {
       external: ['svgo'],
