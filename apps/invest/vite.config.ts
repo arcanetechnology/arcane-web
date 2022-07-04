baef2dd01f4b8a771b44879439248b872f1f0fca
Joel DSouza <joel.dsouza@seamless.se>
2022-07-04 09:32:10 +0200

    fix(invest): updated the dependencies

--- apps/invest/vite.config.ts
+++ apps/invest/vite.config.ts
@@ -7,7 +7,6 @@ import solidSvg from 'vite-plugin-solid-svg';
 export default defineConfig({
   plugins: [solidPlugin(), solidSvg({ defaultExport: 'component' })],
   build: {
-    base: '/invest',
     outDir: './dist/invest',
     target: 'esnext',
     polyfillDynamicImport: false,
