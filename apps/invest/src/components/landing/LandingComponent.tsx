78199b3387a167c319078e37829ca0f1288d5db8
Joel DSouza <joel.dsouza@seamless.se>
2022-07-14 16:32:38 +0200

    feat(invest): added landing page animation

--- apps/invest/src/components/landing/LandingComponent.tsx
+++ apps/invest/src/components/landing/LandingComponent.tsx
@@ -2,19 +2,28 @@
 
 import type { JSXElement, FlowComponent } from 'solid-js';
 import Invest from '../../assets/invest.svg';
+import { Transition } from 'solid-transition-group';
 
 type LandingProps = {
   children: JSXElement;
 };
 
 const Landing: FlowComponent<LandingProps> = (props) => {
+  const base = { opacity: 1 };
+  const options = { duration: 2000 };
+  const animateIn = (el) => {
+    el.animate([{ opacity: 0 }, base], options);
+  };
+
   return (
     <>
       <section class="margin-48">
         <div class="container" style={{ 'text-align': 'center' }}>
-          <h2>
-            Build generational wealth with our actively managed crypto fund.
-          </h2>
+          <Transition onEnter={animateIn} appear={true}>
+            <h2>
+              Build generational wealth with our actively managed crypto fund.
+            </h2>
+          </Transition>
         </div>
       </section>
       <section class="margin-48">
