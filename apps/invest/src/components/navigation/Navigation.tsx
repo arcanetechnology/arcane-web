cc24a6ed097c2e238005bfe7caf49826a2d5bcfb
Joel DSouza <joel.dsouza@seamless.se>
2022-08-03 09:31:36 +0200

    feat(invest): added navigation functionality

--- apps/invest/src/components/navigation/Navigation.tsx
+++ apps/invest/src/components/navigation/Navigation.tsx
@@ -1,16 +1,8 @@
 /** @format */
 
-import { createSignal, Show, VoidComponent, For } from 'solid-js';
-import { gql } from '@solid-primitives/graphql';
-import client from '../../platform-contentful';
+import { Show, VoidComponent, For, createEffect } from 'solid-js';
 import './Navigation.scss';
 import { getNavigation } from '../../api/navigation';
-
-type NavItems = {
-  logo: { url: string; description: string };
-  name: string;
-  path: null | string;
-};
 
 const NAVIGATION_MENU_ID = 'arcane-header-navigation-menu';
 const NAVIGATION_MENU_BUTTON_ID = 'arcane-header-navigation-menu-button';
@@ -19,12 +11,11 @@ const Navigation: VoidComponent = () => {
   const nav = getNavigation();
 
   const navigationRef: (el: HTMLDivElement) => void = (el: HTMLDivElement) => {
-    const navigationMenu = document.getElementById(NAVIGATION_MENU_ID);
-    const navigationMenuButton = document.getElementById(
-      NAVIGATION_MENU_BUTTON_ID
-    );
-
     document.addEventListener('click', (e: any) => {
+      const navigationMenu = document.getElementById(NAVIGATION_MENU_ID);
+      const navigationMenuButton = document.getElementById(
+        NAVIGATION_MENU_BUTTON_ID
+      );
       const includesMenu = !e.path.includes(navigationMenu);
       const includesMenuBtn = !e.path.includes(navigationMenuButton);
       if (includesMenu && includesMenuBtn) {
@@ -35,26 +26,27 @@ const Navigation: VoidComponent = () => {
     });
 
     el.addEventListener('click', () => {
-      if (navigationMenu) {
-        navigationMenu.style.visibility = 'visible';
+      const getNavigationMenu = document.getElementById(NAVIGATION_MENU_ID);
+      if (getNavigationMenu) {
+        getNavigationMenu.style.visibility = 'visible';
       }
     });
   };
 
   return (
-    <div id="menu-container" class="menu-container" data-is-closed="true">
-      <div
-        ref={navigationRef}
-        id={NAVIGATION_MENU_BUTTON_ID}
-        class="menu-btn circle-hover"
-      >
-        <abbr class="clear-df-abbr" title="Google applications">
+    <div
+      ref={navigationRef}
+      id="menu-container"
+      class="menu-container"
+      data-is-closed="true"
+    >
+      <div id={NAVIGATION_MENU_BUTTON_ID} class="menu-btn circle-hover">
+        <abbr class="clear-df-abbr" title="Arcane applications">
           <svg viewBox="0 0 24 24">
             <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z" />
           </svg>
         </abbr>
       </div>
-
       <div id={NAVIGATION_MENU_ID} class="menu margin-12">
         <Show when={nav()}>
           <nav class="gap-small" data-auto-grid="2">
