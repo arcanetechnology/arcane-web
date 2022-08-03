b3243bfb52e4b9764ea5c263addce7f02b5a4421
Joel DSouza <joel.dsouza@seamless.se>
2022-08-03 16:41:10 +0200

    fix(invest): opacity on route

--- apps/invest/src/components/navigation/Navigation.tsx
+++ apps/invest/src/components/navigation/Navigation.tsx
@@ -4,7 +4,6 @@ import { Show, VoidComponent, For } from 'solid-js';
 import './Navigation.scss';
 import { getNavigation } from '../../api/navigation';
 import menu from '../../assets/menu.svg';
-import { isSmall } from '..';
 import { Authentication } from '@arcane-web/arcane-components';
 
 const NAVIGATION_MENU_ID = 'arcane-header-navigation-menu';
@@ -36,6 +35,8 @@ const Navigation: VoidComponent = () => {
     });
   };
 
+  console.log(window.location.pathname.replaceAll('/', ''));
+
   return (
     <div
       ref={navigationRef}
@@ -53,7 +54,15 @@ const Navigation: VoidComponent = () => {
           <nav class="app-box">
             <For each={nav().applicationCollection.items}>
               {(n) => (
-                <div class="app-nav">
+                <div
+                  class="app-nav"
+                  style={{
+                    opacity:
+                      window.location.pathname.replaceAll('/', '') === n.path
+                        ? 1
+                        : 0.4,
+                  }}
+                >
                   <a
                     class="app-nav-link"
                     href={window.location.origin + '/' + (n.path ?? '')}
