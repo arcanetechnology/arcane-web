4290cca5a8176bd2305d885df7f96b7d970bd889
Joel DSouza <joel.dsouza@seamless.se>
2022-08-10 22:33:05 +0200

    fix(invest): fixed the footer style, made it according to figma

--- apps/invest/src/components/footer/Footer.tsx
+++ apps/invest/src/components/footer/Footer.tsx
@@ -35,7 +35,7 @@ const Footer: VoidComponent = () => {
             />
           </div>
           <div class="footer-navigation">
-            <div style={{ 'margin-right': '96px' }}>
+            <div>
               <p class="heading8">Menu</p>
               <nav id="arcane-application-navigation" class="align-vertical">
                 <Show when={sortedNavigation().length > 0}>
@@ -70,7 +70,7 @@ const Footer: VoidComponent = () => {
                 </Show>
               </nav>
             </div>
-            <div style={{ 'margin-right': '96px' }}>
+            <div>
               <p class="heading8 footer-text">Company</p>
               <nav id="arcane-static" class="align-vertical">
                 <div style={{ 'margin-top': '16px' }}>
@@ -115,15 +115,20 @@ const Footer: VoidComponent = () => {
       <hr />
       <div class="container">
         <div class="sub-footer">
-          <div class="copyright">© All rights reserved to Arcane.</div>
+          <div class="copyright">
+            <p class="body3">© All rights reserved to Arcane.</p>
+          </div>
           <div class="madeIn">
-            <p style={{ 'padding-right': '4px' }}>Made with</p>
-            <img src={heart} width={15} height={19} style={{ opacity: 0.4 }} />
-            <p style={{ 'padding-left': '4px' }}>in Norway</p>
+            <p style={{ 'padding-right': '8px' }} class="body3">
+              Made with
+            </p>
+            <img src={heart} width={16} height={19} style={{ opacity: 0.4 }} />
+            <p style={{ 'padding-left': '8px' }} class="body3">
+              in Norway
+            </p>
           </div>
           <div class="social">
             <Show when={!isSmall()}>
-              {' '}
               <p
                 class="body3"
                 style={{
@@ -160,132 +165,6 @@ const Footer: VoidComponent = () => {
           </div>
         </div>
       </div>
-      {/* <div
-        class="container margin-48 padding-24 gap-small"
-        classList={{
-          'footer-row': !isSmall(),
-          'footer-side': isSmall(),
-          'gap-big': !isSmall(),
-          'gap-small': isSmall(),
-        }}
-      >
-        <div class="padding-4">
-          <img
-            src={Logo}
-            style={{
-              filter: 'invert(1)',
-            }}
-          />
-        </div>
-        <div
-          style={{
-            'flex-grow': 1,
-          }}
-        />
-
-        <div
-          class="padding-4 gap-small footer-row"
-          classList={{
-            'gap-small': isSmall(),
-            'gap-big': !isSmall(),
-          }}
-        >
-          <div>
-            <p class="heading8 footer-text">Menu</p>
-            <nav id="arcane-application-navigation" class="align-vertical">
-              <Show when={nav()}>
-                {
-                  <For each={nav().applicationCollection.items}>
-                    {(n) => (
-                      <div class="footer-text">
-                        <a
-                          class="third after footer-link body1 "
-                          href={window.location.origin + '/' + (n.path ?? '')}
-                        >
-                          {n.name}
-                        </a>
-                      </div>
-                    )}
-                  </For>
-                }
-              </Show>
-            </nav>
-          </div>
-          <div>
-            <p class="heading8 footer-text">Company</p>
-            <nav id="arcane-static" class="align-vertical">
-              <div class="footer-text">
-                <a class="third after footer-link body1" href="/people">
-                  People
-                </a>
-              </div>
-              <div class="footer-text">
-                <a class="third after footer-link body1" href="/relations">
-                  Investor Relations
-                </a>
-              </div>
-              <div class="footer-text">
-                <a class="third after footer-link body1" href="/privacy">
-                  Privacy
-                </a>
-              </div>
-            </nav>
-          </div>
-        </div>
-      </div>
-
-      <hr />
-
-      <div
-        class="container margin-12 padding-16 gap-small"
-        classList={{
-          'footer-row': !isSmall(),
-          'footer-side': isSmall(),
-        }}
-      >
-        <div
-          class="gap-small"
-          classList={{
-            'footer-row': !isSmall(),
-            'footer-side': isSmall(),
-          }}
-        >
-          <p class="body3">© All rights reserved to Arcane.</p>
-          <p class="body3">Made with ❤️ in Norway</p>
-        </div>
-        <div
-          style={{
-            'flex-grow': 1,
-          }}
-        />
-
-        <div class="footer-follow gap-small">
-          <p class="body3">Follow us</p>
-          <a href="https://twitter.com/arcane_crypto" class="footer-link body3">
-            <img
-              src={Twitter}
-              width={20}
-              alt="twitter"
-              style={{
-                filter: 'invert(1)',
-              }}
-            />
-          </a>
-          <a
-            href="https://www.linkedin.com/company/arcane-crypto/"
-            class="body3"
-          >
-            <img
-              src={LinkedIn}
-              width={20}
-              alt="linkedin"
-              style={{
-                filter: 'invert(1)',
-              }}
-            />
-          </a>
-        </div>
-      </div> */}
     </footer>
   );
 };
