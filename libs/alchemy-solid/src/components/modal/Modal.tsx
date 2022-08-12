865bd1de95d3acdc2c3eaadeb5c05ab2e74a3822
Joel DSouza <joel.dsouza@seamless.se>
2022-08-12 10:07:25 +0200

    fix(alchemy-solid): added the close icon for the modal

--- libs/alchemy-solid/src/components/modal/Modal.tsx
+++ libs/alchemy-solid/src/components/modal/Modal.tsx
@@ -32,11 +32,23 @@ const Modal: FlowComponent<ModalProps> = (props) => {
               <Button
                 variant="tertiary"
                 size="medium"
+                style={{ width: '50px' }}
                 type="button"
                 onClick={() => others.toggleModal(false)}
                 class="modal-close"
               >
-                X
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  // eslint-disable-next-line solid/no-unknown-namespaces
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M10 8.586L2.929 1.515L1.515 2.929L8.586 10l-7.071 7.071l1.414 1.414L10 11.414l7.071 7.071l1.414-1.414L11.414 10l7.071-7.071l-1.414-1.414L10 8.586z"
+                  />
+                </svg>
               </Button>
             </div>
           </Show>
