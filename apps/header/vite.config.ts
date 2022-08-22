02040bd0e3897a7ef458f3936d82a853f251cdc5
Joel DSouza <joeldsouzax@gmail.com>
2022-08-22 10:15:58 +0200

    feat(arcane): updated vite config in header

--- apps/header/vite.config.ts
+++ apps/header/vite.config.ts
@@ -6,10 +6,13 @@ import solid from 'vite-plugin-solid';
 export default defineConfig({
   plugins: [solid({ ssr: true })],
   build: {
+    manifest: true,
+    ssrManifest: true,
+    
     rollupOptions: {
       output: {
         entryFileNames: 'header.js',
-      },
-    },
-  },
+       },
+     },
+   },
 });
