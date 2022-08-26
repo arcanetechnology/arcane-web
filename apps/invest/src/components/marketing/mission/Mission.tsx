4dce2f1eedef4d331b90b950b4d0a16f9e60f65e
Joel DSouza <joeldsouzax@gmail.com>
2022-08-26 10:56:54 +0300

    fix(invest): changed bold colors in home mission card

--- apps/invest/src/components/marketing/mission/Mission.tsx
+++ apps/invest/src/components/marketing/mission/Mission.tsx
@@ -3,7 +3,7 @@
 import type { VoidComponent } from 'solid-js';
 import './Mission.scss';
 import SolidRichText from 'rich-text-solid-renderer';
-import { BLOCKS } from '@contentful/rich-text-types';
+import { BLOCKS, MARKS } from '@contentful/rich-text-types';
 
 type MissionProps = {
   url: string;
@@ -11,14 +11,14 @@ type MissionProps = {
 };
 
 const options = {
+  renderMark: {
+    [MARKS.BOLD]: (props) => (
+      <span style={{ color: '#495057' }}>{props.children}</span>
+    ),
+  },
   renderNode: {
     [BLOCKS.PARAGRAPH]: (props) => {
-      return (
-        <>
-          <br />
-          <p class="small">{props.children}</p>
-        </>
-      );
+      return <p class="small">{props.children}</p>;
     },
 
     [BLOCKS.HEADING_6]: (props) => {
@@ -32,15 +32,6 @@ const Mission: VoidComponent<MissionProps> = (props) => {
     <div class="container" data-auto-grid="2">
       <img width={600} src={props.url} alt="mission" />
       <div class="align-vertical">
-        {/* <h6 class="contact-marketing">
-          The Arcane Assets Fund is an{' '}
-          <span style={{ color: '#495057' }}>actively managed </span>fund with
-          the mission of capturing the value of{' '}
-          <span style={{ color: '#495057' }}>
-            cryptocurrencies as an asset class
-          </span>{' '}
-          as a whole in the years ahead.
-        </h6> */}
         <SolidRichText document={props.description} options={options} />
       </div>
     </div>
