742f2450394cca54e8956b0ed807d4bd25a7be93
Joel DSouza <joeldsouzax@gmail.com>
2022-09-19 11:19:12 +0200

    feat(trade): added environment variables and types for it

--- admin/trade/src/vite-env.d.ts
+++ admin/trade/src/vite-env.d.ts
@@ -2,3 +2,16 @@
 
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_FIREBASE_API_KEY: string;
+  readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+  readonly VITE_FIREBASE_PROJECT_ID: string;
+  readonly VITE_FIREBASE_STORAGE_BUCKET: string;
+  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+  readonly VITE_FIREBASE_APP_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
