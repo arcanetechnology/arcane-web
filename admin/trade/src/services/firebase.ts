829a676426a5c67d057209c3feaec6da44168baa
Joel DSouza <joeldsouzax@gmail.com>
2022-09-20 16:21:56 +0200

    feat(trade): added firebase config and app

---
+++ admin/trade/src/services/firebase.ts
@@ -0,0 +1,28 @@
+/** @format */
+
+import type { FirebaseOptions } from 'firebase/app';
+import firebase from 'firebase/app'; // include the Firebase module
+import {
+  getAuth,
+  onAuthStateChanged,
+  getRedirectResult,
+  signInWithRedirect,
+  SAMLAuthProvider,
+  signOut,
+} from 'firebase/auth';
+
+const config: FirebaseOptions = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+const app = firebase.initializeApp(config);
+const auth = getAuth(app);
+const provider = new SAMLAuthProvider(import.meta.env.VITE_SAML_PROVIDER_ID);
+
+export { auth, provider, signInWithRedirect, getRedirectResult, signOut };
