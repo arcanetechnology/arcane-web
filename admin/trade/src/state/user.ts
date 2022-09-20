60fb274697215e05b315f3f08133ac72f11693f4
Joel DSouza <joeldsouzax@gmail.com>
2022-09-20 16:22:47 +0200

    feat(trade): added user state and reducer to store firebase state

--- admin/trade/src/state/user.ts
+++ admin/trade/src/state/user.ts
@@ -1,1 +1,35 @@
 /** @format */
+
+import {
+  CaseReducer,
+  createSlice,
+  PayloadAction,
+  SliceCaseReducers,
+} from '@reduxjs/toolkit';
+import type { User } from 'firebase/auth';
+
+type UserState = {
+  user: User | null;
+};
+
+type Login = CaseReducer<UserState, PayloadAction<User>>;
+type Logout = CaseReducer<UserState>;
+
+const login: Login = (state, action) => {
+  state.user = action.payload;
+};
+
+const logout: Logout = (state) => {
+  state.user = null;
+};
+
+const userSlice = createSlice<UserState, SliceCaseReducers<UserState>, 'user'>({
+  name: 'user',
+  initialState: {
+    user: null,
+  } as UserState,
+  reducers: {
+    login,
+    logout,
+  },
+});
