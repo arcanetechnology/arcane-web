376ac1f7c4a016d598237f8e47658fb817cfa914
Joel DSouza <joeldsouzax@gmail.com>
2022-09-22 10:43:05 +0200

    feat(trade): cleaned up the style for account cards list

--- admin/trade/src/components/accounts/AccountCard.tsx
+++ admin/trade/src/components/accounts/AccountCard.tsx
@@ -4,14 +4,12 @@ import { GAP } from '@/constants';
 import { StakeholderFiatAccount } from '@/types/backend';
 import {
   Card,
-  CardActionArea,
   CardContent,
   Box,
   Typography,
-  CardMedia,
   CardActions,
   Button,
-  Link,
+  Divider,
 } from '@mui/material';
 import * as React from 'react';
 import { NavLink } from 'react-router-dom';
@@ -25,20 +23,23 @@ const AccountCard: React.FC<ProfileCardProps> = ({ account }) => {
     <Card component={Box} minWidth={275} mr={GAP} variant="outlined">
       <CardContent>
         <Typography variant="subtitle2">currency</Typography>
-        <Typography variant="h4" gutterBottom component="div">
+        <Typography variant="h4" component="div">
           {account.currency}
         </Typography>
+        <Divider sx={{ mb: 2 }} />
         <Typography variant="subtitle2">balance</Typography>
-        <Typography variant="h4" gutterBottom component="div">
+        <Typography variant="h4" component="div">
           {account.balance}
         </Typography>
+        <Divider sx={{ mb: 2 }} />
         <Typography variant="subtitle2">alias</Typography>
-        <Typography variant="h4" gutterBottom component="div">
+        <Typography variant="h4" component="div">
           {account.alias}
         </Typography>
       </CardContent>
       <CardActions>
         <Button
+          variant="contained"
           component={NavLink}
           LinkComponent={NavLink}
           to={account.id + '/portfolios'}
@@ -46,7 +47,14 @@ const AccountCard: React.FC<ProfileCardProps> = ({ account }) => {
         >
           Check Details
         </Button>
-        <Link component={Button}>{account.custodyAccountId}</Link>
+        <Button
+          component={NavLink}
+          LinkComponent={NavLink}
+          to={account.id + '/portfolios'}
+          size="small"
+        >
+          Custody Account
+        </Button>
       </CardActions>
     </Card>
   );
