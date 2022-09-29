016924031de85d096f89f638ada16fcabd53333e
Joel DSouza <joeldsouzax@gmail.com>
2022-09-29 11:30:26 +0200

    refactor(trade): cleaned up unwanted code

--- admin/trade/src/constants/index.ts
+++ admin/trade/src/constants/index.ts
@@ -1,7 +1,5 @@
 /** @format */
 
-import { CryptoPath } from '@/types/frontend';
-
 // api
 export const USERS_ENDPOINT = 'users';
 export const PROFILES_ENDPOINT = 'profiles';
@@ -9,31 +7,6 @@ export const ACCOUNTS_ENDPOINT = 'accounts';
 export const PORTFOLIOS_ENDPOINT = 'portfolios';
 export const CRYPTOS_ENDPOINT = 'cryptos';
 export const CUSTODY_ENDPOINT = 'custodies';
-
-// api util function to create paths
-const endpoints = {
-  user: 'users',
-  profile: 'profiles',
-  account: 'accounts',
-  portfolio: 'portfolios',
-  crypto: 'cryptos',
-};
-
-export const getPath = (
-  path: Partial<CryptoPath> = {},
-  key: keyof typeof endpoints | null = null
-) => {
-  const pathList = Object.values(endpoints);
-  const idList = Object.values(path);
-  const endpoint = idList.map((id, i) => {
-    return pathList[i] + '/' + id;
-  });
-
-  if (key) {
-    return endpoint.join('/') + '/' + endpoints[key];
-  }
-  return endpoints['user'];
-};
 
 // UI
 export const GAP = 2;
---
+++ admin/trade/src/routes/brokers/index.ts
@@ -0,0 +1,3 @@
+/** @format */
+
+export { default as ViewBrokers } from './view-brokers';
